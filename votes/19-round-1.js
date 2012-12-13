1cb829d34de36f4aec6ce07d82f1dd7aed3523fc
Hatem Mahmoud <hatem@expressionlab.com>
2012-12-13 17:44:30 +0200

    Changed choices and sample votes

--- votes/19-round-1.js
+++ votes/19-round-1.js
@@ -1,9 +1,9 @@
 {
   "table": { 
-    "cols": ["VoteCount-1", "VoteCount-2", "VoteCount-3", "VoteCount-4", "VoteCount-5", "VoteCount-6", "VoteCount-7", "VoteCount-8", "VoteCount-9", "VoteCount-10", "VoteCount-11", "VoteCount-12", "VoteCount-13", "ID", "ValidVotesCount"], 
+    "cols": ["VoteCount-1", "VoteCount-2", "ID", "ValidVotesCount"], 
     "rows": [ 
-              [ 10, 10, 10, 10, 200, 10, 10, 10, 10, 100, 10, 200, 300, "256", 890 ],
-              [ 0, 0, 0, 0, 200, 0, 0, 0, 0, 0, 0, 0, 0, "3751", 200 ]
+              [ 500000,  2000000, "256",  2500000 ],
+              [ 1500000, 1000000, "3751", 2500000 ]
             ] 
   } 
 }
